17196eced15fb7802c5d8c2b75c5f15e8d58d982
Jake Soenneker <jake@soenneker.com>
2025-07-28 08:04:24 -0500

    Initial commit

---
+++ src/Registrars/LoopsOpenApiSuiteRegistrar.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Soenneker.Loops.Contacts.Registrars;
+
+namespace Soenneker.Loops.OpenApi.Suite.Registrars;
+
+/// <summary>
+/// A comprehensive suite of utility libraries for Loop.so's OpenApi operations
+/// </summary>
+public static class LoopsOpenApiSuiteRegistrar
+{
+    public static IServiceCollection AddLoopsOpenApiSuiteAsSingleton(this IServiceCollection services)
+    {
+        services.AddLoopsContactsUtilAsSingleton();
+
+        return services;
+    }
+
+    public static IServiceCollection AddLoopsOpenApiSuiteAsScoped(this IServiceCollection services)
+    {
+        services.AddLoopsContactsUtilAsScoped();
+
+        return services;
+    }
+}
---
+++ test/Soenneker.Loops.OpenApi.Suite.Tests/LoopsOpenApiSuiteTests.cs
@@ -0,0 +1,18 @@
+﻿using Soenneker.Tests.FixturedUnit;
+using Xunit;
+
+namespace Soenneker.Loops.OpenApi.Suite.Tests;
+
+[Collection("Collection")]
+public sealed class LoopsOpenApiSuiteTests : FixturedUnitTest
+{
+    public LoopsOpenApiSuiteTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    {
+    }
+
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
